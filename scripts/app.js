d9426031462bfebd7b39561c6b0be1fa08eb5f56
Yossi Kolesnicov <yossik@fortscale.com>
2014-08-16 00:30:37 +0300

    Alerting error.

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,21 @@ app = angular.module("Diary", ["Tap", "ToggleDisplay", "EventBus", "Utils", "pas
             .connection('diaryDB')
             .upgradeDatabase(2, function(event, db, tx){
                 if (event.newVersion > event.oldVersion) {
-                    db.deleteObjectStore("entries");
-                    var objStore = db.createObjectStore('entries', { autoIncrement: true});
-                    objStore.createIndex('type_idx', 'type', {unique: false});
-                    objStore.createIndex('date_idx', 'date', {unique: false});
+                    try {
+                        db.deleteObjectStore("entries");
+                    }
+                    catch(e){
+                        alert("Can't delete store");
+                    }
+
+                    try {
+                        var objStore = db.createObjectStore('entries', { autoIncrement: true});
+                        objStore.createIndex('type_idx', 'type', {unique: false});
+                        objStore.createIndex('date_idx', 'date', {unique: false});
+                    }
+                    catch(error){
+                        alert("can't create store: " + JSON.stringify(error))
+                    }
                 }
             });
     });
