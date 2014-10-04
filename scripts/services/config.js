28325e6133e5c73063cf269cea23c530696d58d2
Yossi Kolesnicov <yossik@fortscale.com>
2014-10-04 17:55:39 +0300

    Added several milestones.

--- scripts/services/config.js
+++ scripts/services/config.js
@@ -10,11 +10,17 @@ app.factory("config", ["utils", function(utils){
         milestone: {
             types: [
                 { id: "smile", text: "Smiled for the first time! :)" },
+                { id: "turnedOver", text: "Turned over for the first time!)" },
                 { id: "laughter", text: "Laughed for the first time! :D" },
+                { id: "solids", text: "Has started eating solids." },
                 { id: "crawling", text: "Has started crawling!" },
+                { id: "sitting", text: "Sat up for the first time!!" },
                 { id: "word", text: "Said:" },
                 { id: "potty", text: "Used the potty for the first time!" },
-                { id: "walking", text: "Started walking!" }
+                { id: "haircut", text: "Had the first haircut!" },
+                { id: "walking", text: "Started walking!" },
+                { id: "daycare", text: "Has started going to daycare" },
+                { id: "diapers", text: "Is not using diapers anymore!" }
             ]
         }
     };
